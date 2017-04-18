c3ee29a665c3c8f466ee1e249f51d6b21417580f
Matthew Milstead <mattgmilstead@gmail.com>
2017-04-18 16:52:11 -0400

    Adding Customer MVC framework

---
+++ InventoryManagementSystem/Controllers/CustomerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryManagementSystem.Controllers
+{
+    public class CustomerController : Controller
+    {
+        
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InventoryManagementSystem/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagementSystem.Models
+{
+    public class Customer : Person
+    {
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Last Purchase")]
+        public DateTime Purchase { get; set; }
+
+        
+    }
+}
